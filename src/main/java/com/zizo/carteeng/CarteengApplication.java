b7bb7750d6cbdcce874b0724e2c6725ea004059d
thecloer <chltjrrb97@gmail.com>
2024-01-05 14:05:42 +0900

    INIT: spring boot, h2database, jpa, lombok

---
+++ src/main/java/com/zizo/carteeng/CarteengApplication.java
@@ -0,0 +1,13 @@
+package com.zizo.carteeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zizo/carteeng/CarteengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zizo.carteeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
