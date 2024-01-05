468db451be0e312c6cd4b257552c4b53a09b4bbb
thecloer <chltjrrb97@gmail.com>
2024-01-05 17:21:50 +0900

    feat: authFilter template

--- src/main/java/com/zizo/carteeng/CarteengApplication.java
+++ src/main/java/com/zizo/carteeng/CarteengApplication.java
@@ -1,7 +1,5 @@
 package com.zizo.carteeng;
 
-import com.zizo.carteeng.service.MeetupService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
---
+++ src/main/java/com/zizo/carteeng/filter/AuthFilter.java
@@ -0,0 +1,41 @@
+package com.zizo.carteeng.filter;
+
+import com.zizo.carteeng.domain.Member;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+
+        try{
+
+//            System.out.println(session.getId());
+//            System.out.println(session.getAttribute("status"));
+//            Member member = (Member) session.getAttribute("member");
+//            System.out.println(member);
+
+            chain.doFilter(request, response);
+        }catch(Exception e){
+            throw e;
+        }finally {
+
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
